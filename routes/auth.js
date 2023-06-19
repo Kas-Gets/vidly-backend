30e528ba5e026b0977fb232319f255c0d9edee5b
Kassmasse01 <kasget1@gmail.com>
2023-06-19 15:28:49 +0300

    F10V9-generating authentication tokens

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,8 @@
+const jwt = require('jsonwebtoken');
+const Joi = require('joi');
 const bcrypt = require('bcrypt');
 const _ = require('lodash');
-const {User, } = require('../models/user');
+const {User} = require('../models/user');
 const express = require('express');
 const router = express.Router();
 
@@ -12,10 +14,12 @@ router.post('/', async (req, res) => {
     let user = await User.findOne({ email: req.body.email });
     if (!user) return res.status(400).send('Invalid email or password.');
 
-    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    const validPassword = bcrypt.compare(req.body.password, user.password);
     if (!validPassword) return res.status(400).send('Invalid email or password.');
     
-    res.send(true);
+    const token = jwt.sign({ _id: user._id}, 'jwtPrivateKey');
+
+    res.send(token);
 });
 
 function validate(req) {
